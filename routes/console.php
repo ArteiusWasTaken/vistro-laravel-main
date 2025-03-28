de7763268f781a9c55f5bf1c262c97d9d8954362
Alexander <balexander.rosas@gmail.com>
2025-03-28 16:28:58 -0600

    Cron KeepAlive 2

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@
 use App\Console\Commands\KeepPrinterAlive;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(KeepPrinterAlive::class)->everyThirtySeconds();
+Schedule::command('printer:keep-alive')->everyThirtySeconds();
