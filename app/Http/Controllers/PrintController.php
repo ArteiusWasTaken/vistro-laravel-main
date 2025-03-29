9605c6e3f81864927530abd90e48c20670678c4d
Alexander <balexander.rosas@gmail.com>
2025-03-29 14:13:47 -0600

    See py3

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -7,18 +7,24 @@ use Illuminate\Http\JsonResponse;
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\Printer;
 
+/**
+ *
+ */
 class PrintController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         try {
-            $command = "python python/label/2x1/sku_description.py " .
-                escapeshellarg("FDMVVS69495") . " " .
-                escapeshellarg("Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre") . " " .
-                escapeshellarg("1") . " " .
-                escapeshellarg("Cod. Universal: 8011003809219") . " 2>&1";
-
-            $output = shell_exec($command);
+//            $command = 'python python/label/2x1/sku_description.py ' .
+//                escapeshellarg('FDMVVS69495') . ' ' .
+//                escapeshellarg('Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre') . ' ' .
+//                escapeshellarg('1') . ' ' .
+//                escapeshellarg('Cod. Universal: 8011003809219') . ' 2>&1';
+//
+//            $output = shell_exec($command);
 //            $command = "python python/label/2x1/sku_description_serie.py " .
 //                escapeshellarg("FDMVVS69495") . " " .
 //                escapeshellarg("Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre") . " " .
@@ -27,22 +33,22 @@ class PrintController extends Controller
 //                escapeshellarg("") . " 2>&1";
 //
 //            $output = shell_exec($command);
-//            $command = "python python/label/2x1/description.py " .
-//                escapeshellarg("Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre") . " " .
-//                escapeshellarg("1") . " 2>&1";
-//
-//            $output = shell_exec($command);
+            $command = 'python3 python/label/2x1/description.py ' .
+                escapeshellarg('Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre') . ' ' .
+                escapeshellarg('1') . ' 2>&1';
 
-            $connector = new NetworkPrintConnector("192.168.100.7", 9100);
+            $output = shell_exec($command);
+
+            $connector = new NetworkPrintConnector('192.168.100.7', 9100);
             $printer = new Printer($connector);
 
-            $printer->text(trim(mb_convert_encoding($output, "UTF-8", "auto")));
+            $printer->text(trim(mb_convert_encoding($output, 'UTF-8', 'auto')));
             $printer->close();
-        } catch (Exception $e) {
-            echo 'No se pudo imprimir: ' . $e->getMessage();
+        } catch (Exception $exception) {
+            echo 'No se pudo imprimir: ' . $exception->getMessage();
         }
         return response()->json([
-            'Respuesta' => trim(mb_convert_encoding($output, "UTF-8", "auto"))
+            'Respuesta' => trim(mb_convert_encoding($output, 'UTF-8', 'auto'))
 
         ]);
     }
