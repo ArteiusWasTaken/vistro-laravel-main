147fa6653ff55e53ed140274867d30041903da9b
Alexander <balexander.rosas@gmail.com>
2025-03-31 14:34:50 -0600

    Try OMG 4

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -19,26 +19,14 @@ class PrintController extends Controller
             $ip = '172.16.200.63';
             $port = 9100;
 
-            $command = 'python python/label/2x1/sku_description.py ' .
-                escapeshellarg('FDMVVS69495') . ' ' .
-                escapeshellarg('Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre') . ' ' .
-                escapeshellarg('1') . ' ' .
-                escapeshellarg('Cod. Universal: 8011003809219') . ' 2>&1';
-
-            $output = shell_exec($command);
-//
-//            $command = 'python python/label/2x1/description.py ' .
-//                escapeshellarg('Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre') . ' ' .
-//                escapeshellarg('1') . ' 2>&1';
-//
-//            $output = shell_exec($command);
+            $output = '^XA^CI28^LH0,0^FO25,15^BY2,,0^BCN,55,N,N^FDMVVS69495^FS^FT110,98^A0N,22,22^FH^FDMVVS69495^FS^FT109,98^A0N,22,22^FH^FDMVVS69495^FS^FO22,115^A0N,18,18^FB300,2,0,L^FH^FDPerfume Versace Eros Eau De Toilette 100 Ml Para Hombre^FS^FO22,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO21,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO22,175^A0N,18,18^FH^FDCod. Universal: 8011003809219^FS^FO22,175^A0N,18,18^FH^FD^FS^PQ1,0,1,Y^XZ';
 
             $socket = fsockopen($ip, $port, $errno, $errstr, 5);
             if (!$socket) {
                 throw new Exception("No se pudo conectar a la impresora: $errstr ($errno)");
             }
 
-            fwrite($socket, trim(mb_convert_encoding($output, 'UTF-8', 'auto')));
+            fwrite($socket, $output);
             fclose($socket);
 
             return response()->json([
@@ -73,6 +61,12 @@ class PrintController extends Controller
         }
     }
 }
+
+//$command = 'python python/label/2x1/description.py ' .
+//    escapeshellarg('Perfume Versace Eros Eau De Toilette 100 Ml Para Hombre') . ' ' .
+//    escapeshellarg('1') . ' 2>&1';
+//
+//$output = shell_exec($command);
 
 //            $command = 'python python/label/2x1/sku_description.py ' .
 //                escapeshellarg('FDMVVS69495') . ' ' .
