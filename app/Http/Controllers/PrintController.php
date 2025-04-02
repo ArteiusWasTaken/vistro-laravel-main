758cce6c0c982ff69bae6007429386ee322abbe9
Alexander <balexander.rosas@gmail.com>
2025-04-02 13:52:05 -0600

    Test 2 printers

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -20,7 +20,8 @@ class PrintController extends Controller
             $port = 9100;
 
 //            $output = '^XA^CI28^LH0,0^FO25,15^BY2,,0^BCN,55,N,N^FDMVVS69495^FS^FT110,98^A0N,22,22^FH^FDMVVS69495^FS^FT109,98^A0N,22,22^FH^FDMVVS69495^FS^FO22,115^A0N,18,18^FB300,2,0,L^FH^FDPerfume Versace Eros Eau De Toilette 100 Ml Para Hombre^FS^FO22,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO21,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO22,175^A0N,18,18^FH^FDCod. Universal: 8011003809219^FS^FO22,175^A0N,18,18^FH^FD^FS^PQ1,0,1,Y^XZ';
-            $output = '^XA^FX Top section with logo, name and address.^CF0,60^FO100,50^FDEtiqueta^FS^CF0,30^XZ';
+//            $output = '^XA^FX Top section with logo, name and address.^CF0,60^FO100,50^FDEtiqueta^FS^CF0,30^XZ';
+            $output = '^XA^CI28^LH0,0^FO25,15^BY3,,0^BCN,80,N,N^FDMVVS69495^FS^FT110,130^A0N,33,33^FH^FDMVVS69495^FS^FT109,130^A0N,33,33^FH^FDMVVS69495^FS^FO22,160^A0N,27,27^FB450,2,0,L^FH^FDPerfume Versace Eros Eau De Toilette 100 Ml Para Hombre^FS^FO22,200^A0N,27,27^FB450,1,0,L^FH^FD^FS^FO21,200^A0N,27,27^FB450,1,0,L^FH^FD^FS^FO22,230^A0N,27,27^FH^FDCod. Universal: 8011003809219^FS^FO22,230^A0N,27,27^FH^FD^FS^PQ1,0,1,Y^XZ';
             $socket = fsockopen($ip, $port, $errno, $errstr, 5);
             if (!$socket) {
                 throw new Exception("No se pudo conectar a la impresora: $errstr ($errno)");
@@ -49,7 +50,9 @@ class PrintController extends Controller
             $port = 9100;
 
 //            $output = '^XA^CI28^LH0,0^FO25,15^BY2,,0^BCN,55,N,N^FDMVVS69495^FS^FT110,98^A0N,22,22^FH^FDMVVS69495^FS^FT109,98^A0N,22,22^FH^FDMVVS69495^FS^FO22,115^A0N,18,18^FB300,2,0,L^FH^FDPerfume Versace Eros Eau De Toilette 100 Ml Para Hombre^FS^FO22,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO21,153^A0N,18,18^FB300,1,0,L^FH^FD^FS^FO22,175^A0N,18,18^FH^FDCod. Universal: 8011003809219^FS^FO22,175^A0N,18,18^FH^FD^FS^PQ1,0,1,Y^XZ';
-            $output = '^XA^FX Top section with logo, name and address.^CF0,60^FO100,50^FDTicket^FS^CF0,30^XZ';
+//            $output = '^XA^FX Top section with logo, name and address.^CF0,60^FO100,50^FDTicket^FS^CF0,30^XZ';
+            $output = '^XA^CI28^LH0,0^FO25,15^BY3,,0^BCN,80,N,N^FDMVVS69495^FS^FT110,130^A0N,33,33^FH^FDMVVS69495^FS^FT109,130^A0N,33,33^FH^FDMVVS69495^FS^FO22,160^A0N,27,27^FB450,2,0,L^FH^FDPerfume Versace Eros Eau De Toilette 100 Ml Para Hombre^FS^FO22,200^A0N,27,27^FB450,1,0,L^FH^FD^FS^FO21,200^A0N,27,27^FB450,1,0,L^FH^FD^FS^FO22,230^A0N,27,27^FH^FDCod. Universal: 8011003809219^FS^FO22,230^A0N,27,27^FH^FD^FS^PQ1,0,1,Y^XZ';
+
             $socket = fsockopen($ip, $port, $errno, $errstr, 5);
             if (!$socket) {
                 throw new Exception("No se pudo conectar a la impresora: $errstr ($errno)");
