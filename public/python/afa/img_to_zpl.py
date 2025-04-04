c4317292d10f6353b9a3f853f19e39082fafb588
Alexander <balexander.rosas@gmail.com>
2025-04-04 13:38:38 -0600

    Test Printer img/pdf to zpl 28

--- public/python/afa/img_to_zpl.py
+++ public/python/afa/img_to_zpl.py
@@ -1,29 +1,29 @@
 import sys
 import os
-from zebrafy import ZebrafyImage  # Asegúrate de que esta sea la clase correcta
+from zebrafy import ZebrafyImage
 from PIL import Image
 
 def convert_image_to_zpl(image_path, label_width=406, label_height=203, invert=True):
     with open(image_path, "rb") as image:
         zpl_string = ZebrafyImage(
             image.read(),
-            format="Z64",            # Formato Z64
-            invert=invert,           # Invertir colores
-            dither=False,            # Desactivar dithering
-            threshold=128,           # Umbral de color
-}            width=label_width,       # Ancho de la etiqueta en puntos (2 pulgadas)
-            height=label_height,     # Alto de la etiqueta en puntos (1 pulgada)
-            pos_x=100,               # Posición en X (ajustar según necesidad)
-            pos_y=100,               # Posición en Y (ajustar según necesidad)
-            rotation=90,             # Rotación en 90 grados
-            string_line_break=80,    # Longitud de las líneas de texto
-            complete_zpl=True,       # Generar el ZPL completo
+            format="Z64",
+            invert=invert,
+            dither=False,
+            threshold=128,
+            width=label_width,
+            height=label_height,
+            pos_x=100,
+            pos_y=100,
+            rotation=90,
+            string_line_break=80,
+            complete_zpl=True,
         ).to_zpl()
 
     with open("output.zpl", "w") as zpl:
         zpl.write(zpl_string)
 
-    zpl_string = zpl_string.replace('\n', '')  # Eliminar saltos de línea innecesarios
+    zpl_string = zpl_string.replace('\n', '')
 
     return zpl_string
 
@@ -38,5 +38,5 @@ if __name__ == "__main__":
     if not os.path.exists(img_file):
         sys.exit(f"Archivo no encontrado: {img_file}")
 
-    zpl_code = convert_image_to_zpl(img_file, label_width=406, label_height=203, invert=True)  # Invertir colores en True
+    zpl_code = convert_image_to_zpl(img_file, label_width=406, label_height=203, invert=True)
     print(zpl_code)
