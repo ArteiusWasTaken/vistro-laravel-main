d7cda4a1107977491c0e15475480ca484b4e2d4e
Alexander <balexander.rosas@gmail.com>
2025-04-04 13:27:17 -0600

    Test Printer img/pdf to zpl 23

--- public/python/afa/pdf_to_zpl.py
+++ public/python/afa/pdf_to_zpl.py
@@ -3,30 +3,31 @@ import sys
 from zebrafy import ZebrafyPDF
 
 def convert_pdf_to_zpl(pdf_path, label_width=406, label_height=203, dpi=203, invert=False):
-   with open(pdf_path, "rb") as pdf:
-       zpl_string = ZebrafyPDF(
-           pdf.read(),
-           format="Z64",            # Formato Z64
-           invert=False,            # No invertir colores
-           dither=False,            # Desactivar dithering
-           threshold=128,           # Umbral de color
-           dpi=203,                 # DPI a 203 (para etiquetas térmicas)
-           width=406,               # Ancho de la etiqueta en puntos (2 pulgadas)
-           height=203,              # Alto de la etiqueta en puntos (1 pulgada)
-           pos_x=0,                 # Posición en X (ajustar según necesidad)
-           pos_y=0,                 # Posición en Y (ajustar según necesidad)
-           rotation=0,              # No rotar la imagen
-           string_line_break=80,    # Longitud de las líneas de texto
-           complete_zpl=True,       # Generar el ZPL completo
-           split_pages=True,        # Dividir las páginas si el PDF tiene más de una
-       ).to_zpl()
+    with open(pdf_path, "rb") as pdf:
+        zpl_string = ZebrafyPDF(
+            pdf.read(),
+            format="Z64",            # Formato Z64
+            invert=invert,           # No invertir colores
+            dither=False,            # Desactivar dithering
+            threshold=128,           # Umbral de color
+            dpi=dpi,                 # DPI a 203 (para etiquetas térmicas)
+            width=label_width,       # Ancho de la etiqueta en puntos (2 pulgadas)
+            height=label_height,     # Alto de la etiqueta en puntos (1 pulgada)
+            pos_x=0,                 # Posición en X (ajustar según necesidad)
+            pos_y=0,                 # Posición en Y (ajustar según necesidad)
+            rotation=0,              # No rotar la imagen
+            string_line_break=80,    # Longitud de las líneas de texto
+            complete_zpl=True,       # Generar el ZPL completo
+            split_pages=True,        # Dividir las páginas si el PDF tiene más de una
+        ).to_zpl()
 
-   with open("output.zpl", "w") as zpl:
-       zpl.write(zpl_string)
+    with open("output.zpl", "w") as zpl:
+        zpl.write(zpl_string)
 
-   zpl_string = zpl_string.replace('\n', '')
+    # Eliminar saltos de línea innecesarios
+    zpl_string = zpl_string.replace('\n', '')
 
-   return zpl_string
+    return zpl_string
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
