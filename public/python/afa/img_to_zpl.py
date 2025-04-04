50aaa99778950fd64d0ce0d9db58904bd92847b7
Alexander <balexander.rosas@gmail.com>
2025-04-04 13:35:35 -0600

    Test Printer img/pdf to zpl 25

--- public/python/afa/img_to_zpl.py
+++ public/python/afa/img_to_zpl.py
@@ -2,12 +2,41 @@ import sys
 from zebrafy import ZebrafyZPL
 from PIL import Image
 
-def convert_image_to_zpl(image_path):
-    image = Image.open(image_path).convert("1")
-    zpl_label = ZebrafyZPL(image)  # Aquí va directamente la imagen
-    return zpl_label.to_zpl()
+def convert_image_to_zpl(image_path, label_width=406, label_height=203, dpi=203, invert=True):
+   with open(image_path, "rb") as image:
+       zpl_string = ZebrafyImage(
+           image.read(),
+           format="Z64",
+           invert=True,
+           dither=False,
+           threshold=128,
+           dpi=dpi,
+           width=label_width,
+           height=label_height,
+           pos_x=100,
+           pos_y=100,
+           rotation=90,
+           string_line_break=80,
+           complete_zpl=True,
+       ).to_zpl()
+
+    with open("output.zpl", "w") as zpl:
+        zpl.write(zpl_string)
+
+    zpl_string = zpl_string.replace('\n', '')
+
+    return zpl_string
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Uso: python img_to_zpl.py <imagen>")
-        sys.exit(1)
+        sys.exit("Uso: python pdf_to_zpl.py <ruta/relativa/al/pdf>")
+
+    relative_path = sys.argv[1]
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    img_file = os.path.abspath(os.path.join(project_root, relative_path))
+
+    if not os.path.exists(img_file):
+        sys.exit(f"Archivo no encontrado: {img_file}")
+
+    zpl_code = convert_pdf_to_zpl(img_file, label_width=406, label_height=203, invert=True)
+    print(zpl_code)
--- public/python/afa/pdf_to_zpl.py
+++ public/python/afa/pdf_to_zpl.py
@@ -2,29 +2,28 @@ import os
 import sys
 from zebrafy import ZebrafyPDF
 
-def convert_pdf_to_zpl(pdf_path, label_width=406, label_height=203, dpi=203, invert=True):
+def convert_pdf_to_zpl(pdf_path, label_width=406, label_height=203, dpi=203, invert=False):
     with open(pdf_path, "rb") as pdf:
         zpl_string = ZebrafyPDF(
             pdf.read(),
-            format="Z64",            # Formato Z64
-            invert=True,           # No invertir colores
-            dither=False,            # Desactivar dithering
-            threshold=128,           # Umbral de color
-            dpi=dpi,                 # DPI a 203 (para etiquetas térmicas)
-            width=label_width,       # Ancho de la etiqueta en puntos (2 pulgadas)
-            height=label_height,     # Alto de la etiqueta en puntos (1 pulgada)
-            pos_x=0,                 # Posición en X (ajustar según necesidad)
-            pos_y=0,                 # Posición en Y (ajustar según necesidad)
-            rotation=0,              # No rotar la imagen
-            string_line_break=80,    # Longitud de las líneas de texto
-            complete_zpl=True,       # Generar el ZPL completo
-            split_pages=True,        # Dividir las páginas si el PDF tiene más de una
+            format="Z64",
+            invert=invert,
+            dither=False,
+            threshold=128,
+            dpi=dpi,
+            width=label_width,
+            height=label_height,
+            pos_x=0,
+            pos_y=0,
+            rotation=0,
+            string_line_break=80,
+            complete_zpl=True,
+            split_pages=True,
         ).to_zpl()
 
     with open("output.zpl", "w") as zpl:
         zpl.write(zpl_string)
 
-    # Eliminar saltos de línea innecesarios
     zpl_string = zpl_string.replace('\n', '')
 
     return zpl_string
@@ -40,6 +39,5 @@ if __name__ == "__main__":
     if not os.path.exists(pdf_file):
         sys.exit(f"Archivo no encontrado: {pdf_file}")
 
-    # Llama a la función y luego imprime el resultado
-    zpl_code = convert_pdf_to_zpl(pdf_file, label_width=406, label_height=203, invert=False)
+    zpl_code = convert_pdf_to_zpl(pdf_file, label_width=406, label_height=203, invert=True)
     print(zpl_code)
