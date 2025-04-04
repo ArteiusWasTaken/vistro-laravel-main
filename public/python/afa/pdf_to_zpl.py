b7f89b7430a1da043020b0980ea71612b4e6e066
Alexander <balexander.rosas@gmail.com>
2025-04-04 13:03:11 -0600

    Test Printer img/pdf to zpl 9

--- public/python/afa/pdf_to_zpl.py
+++ public/python/afa/pdf_to_zpl.py
@@ -8,17 +8,18 @@ def convert_pdf_to_zpl(pdf_path):
 
     pdf = ZebrafyPDF(pdf_bytes)
     images = pdf.convert()
-
     zpl = ZebrafyZPL(images[0])
     return zpl.to_zpl()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        sys.exit("Uso: python pdf_to_zpl.py <archivo.pdf>")
+        sys.exit("Uso: python pdf_to_zpl.py <ruta/relativa/al/pdf>")
 
     relative_path = sys.argv[1]
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    pdf_file = os.path.abspath(os.path.join(script_dir, "..", "..", relative_path))
+
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+    pdf_file = os.path.abspath(os.path.join(project_root, relative_path))
 
     if not os.path.exists(pdf_file):
         sys.exit(f"Archivo no encontrado: {pdf_file}")
