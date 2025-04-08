ed37ffb3ea61df7dc8197e3e9af6c51a846a2256
Jesus Alberto Gonzalez Lozano <jgonzalezl@northware.mx>
2025-04-07 22:30:31 -0600

    .

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -208,7 +208,7 @@ class PrintController extends Controller
             $printer->text("----------------\n");
             
             // 6. Configurar código de barras (sin constantes)
-            $barcodeData = "1534134"; // Tus datos numéricos
+            $barcodeData = "1534134111"; // Tus datos numéricos
             
             // Configuración del código de barras:
             // - 65 = CODE128-A (caracteres estándar)
