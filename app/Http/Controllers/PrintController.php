3d575bc2b2ed0df082d4bccfb00904d786c95c55
Jesus Alberto Gonzalez Lozano <jgonzalezl@northware.mx>
2025-04-07 22:30:57 -0600

    .

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -217,7 +217,7 @@ class PrintController extends Controller
             
             // Configurar CODE39 (69 es el tipo numérico para CODE39)
             $printer->setBarcodeHeight(80);
-            $printer->setBarcodeWidth(3);
+            //$printer->setBarcodeWidth(3);
             $printer->setBarcodeTextPosition(2); // Texto debajo
             
             // Imprimir código de barras CODE39
