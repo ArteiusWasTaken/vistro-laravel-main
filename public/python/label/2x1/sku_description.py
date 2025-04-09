bf7c219e1db02a6d83377e89343f5e4c73f249c9
Alexander <balexander.rosas@gmail.com>
2025-04-09 12:02:18 -0600

    Impresoras, try etiquetas 2x1 11

--- public/python/label/2x1/sku_description.py
+++ public/python/label/2x1/sku_description.py
@@ -15,7 +15,7 @@ sku_length = len(sku)
 barcode_width_length = 0.2 if sku_length <= 14 else 0.1
 
 # Ajusta esta variable para mover el código de barras a la izquierda o derecha
-barcode_shift = 5  # Aumentar para mover a la derecha, disminuir para mover a la izquierda
+barcode_shift = 1  # Aumentar para mover a la derecha, disminuir para mover a la izquierda
 
 barcode_start_cords_x = (4 if sku_length <= 14 else 15) + barcode_shift
 
